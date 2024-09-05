ac81547568fceaf175031ef6cd661222f3457406
severales <henrique.santana.piologo@gmail.com>
2024-09-05 16:02:43 -0300

    Tipos de variaveis primitivas

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos de variaveis no Java
+        
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        System.out.println(numero);
+        numero = 10;
+
+        System.out.println(numero);
+        
+        final double valorDePi = 3.14;
     }
 }
